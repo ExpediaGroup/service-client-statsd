48169b2df8af0aead459a7da08f75fff1d92e583
Ian White <iwhite@homeaway.com>
2019-12-27 14:29:05 -0600

    Other: Fix code per eslint recommendations

--- tests/unit/index.test.js
+++ tests/unit/index.test.js
@@ -24,7 +24,7 @@ describe('metrics', function () {
 
     ServiceClient.mergeConfig({
       overrides: {
-        'myservice': {
+        myservice: {
           hostname: 'service.local'
         }
       }
